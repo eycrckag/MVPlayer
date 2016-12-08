4fd4d20e2d901bda2321b292e07e3c4a4e7d96a1
Leon <526247082@qq.com>
2016-12-08 11:10:22 +0800

    Custom action bar of settings.

--- app/src/main/java/com/itheima/mvplayer/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/itheima/mvplayer/ui/activity/SettingsActivity.java
@@ -3,7 +3,9 @@ package com.itheima.mvplayer.ui.activity;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.support.v7.app.ActionBar;
 import android.view.LayoutInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
@@ -15,6 +17,25 @@ public class SettingsActivity extends BaseActivity {
     @Override
     public int getLayoutResID() {
         return R.layout.activity_settings;
+    }
+
+    @Override
+    protected void init() {
+        super.init();
+        //Becareful, we need to use getSupportActionBar instead of getActionBar
+        ActionBar supportActionBar = getSupportActionBar();
+        supportActionBar.setTitle(R.string.settings);
+        supportActionBar.setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+        }
+        return true;
     }
 
     public static class SettingsFragment extends PreferenceFragment {

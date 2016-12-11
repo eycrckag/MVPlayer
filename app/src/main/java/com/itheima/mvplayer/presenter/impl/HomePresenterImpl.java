5ea0f4b9841d3194678001a6d089c852ed4d8ed8
Leon <526247082@qq.com>
2016-12-11 18:14:08 +0800

    Refactor HomeFragment.

--- app/src/main/java/com/itheima/mvplayer/presenter/impl/HomePresenterImpl.java
+++ app/src/main/java/com/itheima/mvplayer/presenter/impl/HomePresenterImpl.java
@@ -4,81 +4,81 @@ import com.itheima.mvplayer.model.HomeItemBean;
 import com.itheima.mvplayer.network.HomeRequest;
 import com.itheima.mvplayer.network.MVPlayerRequest;
 import com.itheima.mvplayer.network.NetworkListener;
-import com.itheima.mvplayer.presenter.HomePresenter;
-import com.itheima.mvplayer.view.HomeView;
+import com.itheima.mvplayer.presenter.BaseListPresenter;
+import com.itheima.mvplayer.view.BaseListView;
 
 import java.util.ArrayList;
 import java.util.List;
 
 
-public class HomePresenterImpl implements HomePresenter {
+public class HomePresenterImpl implements BaseListPresenter<HomeItemBean> {
     public static final String TAG = "HomePresenterImpl";
 
-    private HomeView mHomeView;
+    private BaseListView mHomeView;
 
     private List<HomeItemBean> mHomeItemBeanList;
 
     private boolean mHasMoreData = true;
 
-    public HomePresenterImpl(HomeView homeView) {
+    public HomePresenterImpl(BaseListView homeView) {
         mHomeView = homeView;
         mHomeItemBeanList = new ArrayList<HomeItemBean>();
     }
 
     @Override
-    public void loadHomeData() {
+    public void loadListData() {
         if (mHomeItemBeanList.size() > 0) {
-            mHomeView.onLoadHomeDataSuccess();
+            mHomeView.onLoadListDataSuccess();
             return;
         }
         HomeRequest.getRequest(mListNetworkListener).execute();
     }
 
+
     private NetworkListener<List<HomeItemBean>> mListNetworkListener = new NetworkListener<List<HomeItemBean>>() {
         @Override
         public void onError(String errorMsg) {
-            mHomeView.onLoadHomeDataFailed();
+            mHomeView.onLoadListDataFailed();
         }
 
         @Override
         public void onSuccess(List<HomeItemBean> result) {
             mHomeItemBeanList.addAll(result);
-            mHomeView.onLoadHomeDataSuccess();
+            mHomeView.onLoadListDataSuccess();
         }
     };
 
     @Override
-    public List<HomeItemBean> getHomeListItems() {
+    public List<HomeItemBean> getListData() {
         return mHomeItemBeanList;
     }
 
     @Override
     public void refresh() {
         mHomeItemBeanList.clear();
-        loadHomeData();
+        loadListData();
     }
 
     @Override
-    public void loadMoreHomeData() {
+    public void loadMoreListData() {
         if (mHasMoreData) {
             HomeRequest.getLoadMoreRequest(mHomeItemBeanList.size(), mLoadMoreListener).execute();
         } else {
             mHomeView.onNoMoreData();
         }
-
     }
 
     private NetworkListener<List<HomeItemBean>> mLoadMoreListener = new NetworkListener<List<HomeItemBean>>() {
         @Override
         public void onError(String errorMsg) {
-            mHomeView.onLoadMoreHomeDataFailed();
+            mHomeView.onLoadMoreListDataFailed();
         }
 
         @Override
         public void onSuccess(List<HomeItemBean> result) {
             mHasMoreData = (result.size() == MVPlayerRequest.DEFAULT_PAGE_SIZE);
             mHomeItemBeanList.addAll(result);
-            mHomeView.onLoadMoreHomeDataSuccess();
+            mHomeView.onLoadMoreListDataSuccess();
         }
     };
 
--- app/src/main/java/com/itheima/mvplayer/ui/fragment/HomeFragment.java
+++ app/src/main/java/com/itheima/mvplayer/ui/fragment/HomeFragment.java
@@ -1,98 +1,24 @@
 package com.itheima.mvplayer.ui.fragment;
 
-import android.support.v4.widget.SwipeRefreshLayout;
-import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
-import com.itheima.mvplayer.R;
-import com.itheima.mvplayer.presenter.HomePresenter;
+import com.itheima.mvplayer.presenter.BaseListPresenter;
 import com.itheima.mvplayer.presenter.impl.HomePresenterImpl;
 import com.itheima.mvplayer.ui.adapter.HomeListAdapter;
-import com.itheima.mvplayer.view.HomeView;
+import com.itheima.mvplayer.view.BaseListView;
 
-import butterknife.BindView;
+public class HomeFragment extends BaseListFragment {
 
-public class HomeFragment extends BaseFragment implements HomeView {
-    public static final String TAG = "HomeFragment";
-    @BindView(R.id.recycler_view)
-    RecyclerView mRecyclerView;
-    @BindView(R.id.swipe_refresh)
-    SwipeRefreshLayout mSwipeRefresh;
-
-    private LinearLayoutManager mLinearLayoutManager;
-    private HomeListAdapter mHomeListAdapter;
-
-    private HomePresenter mHomePresenter;
+    private HomePresenterImpl mBaseListPresenter;
 
     @Override
-    protected int getLayoutResID() {
-        return R.layout.fragment_home;
+    public RecyclerView.Adapter getListAdapter() {
+        return new HomeListAdapter(getContext(), mBaseListPresenter.getListData());
     }
 
     @Override
-    protected void init() {
-        super.init();
-        mHomePresenter = new HomePresenterImpl(this);
-
-        initRecyclerView();
-        mSwipeRefresh.setColorSchemeResources(R.color.colorPrimary);
-        mSwipeRefresh.setOnRefreshListener(mOnRefreshListener);
-        mHomePresenter.loadHomeData();
-    }
-
-    private void initRecyclerView() {
-        mRecyclerView.setHasFixedSize(true);
-        mLinearLayoutManager = new LinearLayoutManager(getContext());
-        mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mHomeListAdapter = new HomeListAdapter(getContext(), mHomePresenter.getHomeListItems());
-        mRecyclerView.setAdapter(mHomeListAdapter);
-        mRecyclerView.addOnScrollListener(mOnScrollListener);
-    }
-
-    @Override
-    public void onLoadHomeDataFailed() {
-        toast(R.string.load_home_data_failed);
-    }
-
-    @Override
-    public void onLoadHomeDataSuccess() {
-        toast(R.string.load_home_data_success);
-        mHomeListAdapter.notifyDataSetChanged();
-        mSwipeRefresh.setRefreshing(false);
-    }
-
-    private SwipeRefreshLayout.OnRefreshListener mOnRefreshListener = new SwipeRefreshLayout.OnRefreshListener() {
-        @Override
-        public void onRefresh() {
-            mHomePresenter.refresh();
-        }
-    };
-
-
-    private RecyclerView.OnScrollListener mOnScrollListener = new RecyclerView.OnScrollListener() {
-        @Override
-        public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-            if (newState == RecyclerView.SCROLL_STATE_IDLE) {
-                if (mLinearLayoutManager.findLastVisibleItemPosition() == mHomePresenter.getHomeListItems().size() - 1) {
-                    mHomePresenter.loadMoreHomeData();
-                }
-            }
-        }
-    };
-
-    @Override
-    public void onLoadMoreHomeDataFailed() {
-        toast(R.string.load_more_home_data_failed);
-    }
-
-    @Override
-    public void onLoadMoreHomeDataSuccess() {
-        toast(R.string.load_more_home_data_success);
-        mHomeListAdapter.notifyDataSetChanged();
-    }
-
-    @Override
-    public void onNoMoreData() {
-        toast(R.string.no_more_data);
+    public BaseListPresenter getPresenter(BaseListView view) {
+        mBaseListPresenter = new HomePresenterImpl(view);
+        return mBaseListPresenter;
     }
 }

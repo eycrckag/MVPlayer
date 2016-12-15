4a65e5fb3a8194b4552cf2789fecfbdde0b410c7
Leon <526247082@qq.com>
2016-12-15 11:05:48 +0800

    Store shared preference.

--- app/src/main/java/com/itheima/mvplayer/app/Constant.java
+++ app/src/main/java/com/itheima/mvplayer/app/Constant.java
@@ -10,4 +10,10 @@ public class Constant {
         public static final String AUDIO_PATH = "audio_path";
         public static final String AUDIO_POSITION = "audio_position";
     }
+
+    public static class SP {
+        public static final String PUSH_NOTIFICATION = "push_notification";
+        public static final String NO_WIFI_LOAD_IMAGES = "no_wifi_load_images";
+        public static final String ABOUT = "about";
+    }
 }
--- app/src/main/java/com/itheima/mvplayer/ui/activity/SettingsActivity.java
+++ app/src/main/java/com/itheima/mvplayer/ui/activity/SettingsActivity.java
@@ -1,15 +1,21 @@
 package com.itheima.mvplayer.ui.activity;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.preference.Preference;
 import android.preference.PreferenceFragment;
+import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
 import android.support.v7.app.ActionBar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.itheima.mvplayer.R;
+import com.itheima.mvplayer.app.Constant;
 
 public class SettingsActivity extends BaseActivity {
     public static final String TAG = "SettingsActivity";
@@ -22,10 +28,14 @@ public class SettingsActivity extends BaseActivity {
     @Override
     protected void init() {
         super.init();
-        //Becareful, we need to use getSupportActionBar instead of getActionBar
+        //Be careful, we need to use getSupportActionBar instead of getActionBar
         ActionBar supportActionBar = getSupportActionBar();
         supportActionBar.setTitle(R.string.settings);
         supportActionBar.setDisplayHomeAsUpEnabled(true);
+
+        SharedPreferences defaultSharedPreferences = PreferenceManager.getDefaultSharedPreferences(this);
+        boolean aBoolean = defaultSharedPreferences.getBoolean(Constant.SP.NO_WIFI_LOAD_IMAGES, false);
+        Log.d(TAG, "init: " + aBoolean);
     }
 
     @Override
@@ -41,9 +51,23 @@ public class SettingsActivity extends BaseActivity {
     public static class SettingsFragment extends PreferenceFragment {
 
         @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+        }
+
+        @Override
         public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
             addPreferencesFromResource(R.xml.settings_prefs);
             return super.onCreateView(inflater, container, savedInstanceState);
+        }
+
+        @Override
+        public boolean onPreferenceTreeClick(PreferenceScreen preferenceScreen, Preference preference) {
+            if (preference.getKey().equals(Constant.SP.ABOUT)) {
+                Intent intent = new Intent(getActivity(), AboutActivity.class);
+                startActivity(intent);
+            }
+            return super.onPreferenceTreeClick(preferenceScreen, preference);
         }
     }
 }
